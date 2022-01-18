92709de99591097ba99cef997b823b21d861708c
Insignia <olopezmill08@gmail.com>
2022-01-18 00:27:32 -0500

    Update readme

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ This is a First-Person ray cast game using Lode's tutorial rewritten to C# (http
 
 ## RoadMap
 * This game is currently only in Winforms but it will be changed to monogame to use DirectX
-* Because this is running on winforms, resizing the form to a bigger resolution will drastically lower the fps
+because this is running on winforms and resizing the form to a bigger resolution will drastically lower the fps
+
+## How to use
+* once in game press 'm' to lock cursor and look around with mouse (move with W A S D keys)
+* Currently no sprites are added, if you want to add aprite, you can add sprite values to sprite field and specify which texture index to use.
+Any texture will work but if the image is bigger than 256x256 the extra length will be cut off.
--- RayCastGame/Form1.cs
+++ RayCastGame/Form1.cs
@@ -63,6 +63,7 @@ namespace RayCastGame
         {
             InitializeComponent();
             DoubleBuffered = true;
+            Cursor.Hide();
 
             KeyDown += (s, e) =>
             {
@@ -87,28 +88,27 @@ namespace RayCastGame
             for (int i = 0; i < 8; i++)
                 texture[i] = new uint[texWidth * texHeight];
 
-            #region Textures
-            for (int x = 0; x < texWidth; x++)
-                for (int y = 0; y < texHeight; y++)
-                {
-                    int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
-                    //int xcolor = x * 256 / texWidth;
-                    int ycolor = y * 256 / texHeight;
-                    int xycolor = y * 128 / texHeight + x * 128 / texWidth;
-                    texture[0][64 * y + x] = (uint)(65536 * 254 * ((x != y && x != texWidth - y) ? 1 : 0)); //flat red texture with black cross
-                    texture[1][texWidth * y + x] = (uint)(xycolor + 256 * xycolor + 65536 * xycolor); //sloped greyscale
-                    texture[2][texWidth * y + x] = (uint)(256 * xycolor + 65536 * xycolor); //sloped yellow gradient
-                    texture[3][texWidth * y + x] = (uint)(xorcolor + 256 * xorcolor + 65536 * xorcolor); //xor greyscale
-                    texture[4][texWidth * y + x] = (uint)(256 * xorcolor); //xor green
-                    texture[5][texWidth * y + x] = (uint)(65536 * 192 * ((x % 16 == 0 && y % 16 == 0) ? 1 : 0)); //red bricks
-                    texture[6][texWidth * y + x] = (uint)(65536 * ycolor); //red gradient
-                    texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-                }
+            #region Other Textures
+            //for (int x = 0; x < texWidth; x++)
+            //    for (int y = 0; y < texHeight; y++)
+            //    {
+            //        int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
+            //        //int xcolor = x * 256 / texWidth;
+            //        int ycolor = y * 256 / texHeight;
+            //        int xycolor = y * 128 / texHeight + x * 128 / texWidth;
+            //        texture[0][64 * y + x] = (uint)(65536 * 254 * ((x != y && x != texWidth - y) ? 1 : 0)); //flat red texture with black cross
+            //        texture[1][texWidth * y + x] = (uint)(xycolor + 256 * xycolor + 65536 * xycolor); //sloped greyscale
+            //        texture[2][texWidth * y + x] = (uint)(256 * xycolor + 65536 * xycolor); //sloped yellow gradient
+            //        texture[3][texWidth * y + x] = (uint)(xorcolor + 256 * xorcolor + 65536 * xorcolor); //xor greyscale
+            //        texture[4][texWidth * y + x] = (uint)(256 * xorcolor); //xor green
+            //        texture[5][texWidth * y + x] = (uint)(65536 * 192 * ((x % 16 == 0 && y % 16 == 0) ? 1 : 0)); //red bricks
+            //        texture[6][texWidth * y + x] = (uint)(65536 * ycolor); //red gradient
+            //        texture[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
+            //    }
             #endregion
 
-            //LoadTexture(@"C:\Users\SIG\Pictures\Saved Pictures\Enemy.jpg", 0);
             LoadTexture(@"Textures\wall.png", 0);
-            //LoadTexture(@"C:\Users\SIG\Desktop\code\C++Learning\mosnter.png", 1);
+            //LoadTexture(@"Textures\", 1);
             LoadTexture(@"Textures\floor.jpg", 2);
             LoadTexture(@"Textures\ceiling.jpg", 3);
 
